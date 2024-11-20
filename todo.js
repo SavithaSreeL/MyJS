2994c2456a42017fcb2c3e0d636d8cf836b4b4ac
Savitha Sree L <savithasree.l@saint-gobain.com>
2024-11-21 00:02:13 +0530

    final

--- todo.js
+++ todo.js
@@ -1,13 +1,26 @@
 // todo.js
 
-const valuearray = JSON.parse(localStorage.getItem("savedArr")) || [];
-let pendingFlag = false ;
-let completedFlag = false ;
+let valuearray = JSON.parse(localStorage.getItem("savedArr")) || [];
+let pendingFlag = false;
+let completedFlag = false;
+document.getElementById("add").disabled = true;
+let rowId = 0;
+
+
+function generateRowId() {
+  return ++rowId;
+}
+function enableAdd(){
+    category = document.getElementById("drop1").value;;
+    description = document.getElementById("desc1").value;
+    if(category && description){
+        document.getElementById("add").disabled = false;
+    }
+}
 function addTask() {
     debugger;
-    const category = document.getElementById("drop1").value;
-    const description = document.getElementById("desc1").value;
-    let tempArr = { category: category, desc: description, status: false }
+    generateRowId();
+    let tempArr = { rowid:rowId,category: category, desc: description, status: false }
     valuearray.push(tempArr);
     console.log('values', valuearray)
     localStorage.setItem("savedArr", JSON.stringify(valuearray));
@@ -17,14 +30,25 @@ function addTask() {
 function resetValues() {
     document.getElementById("drop1").value = '';
     document.getElementById("desc1").value = null;
+    document.getElementById("add").disabled = true;
+    document.getElementById("rad1").checked = true;
+
 }
-function CheckFunction(item) {
-    valuearray[item].status = !valuearray[item].status;
+function CheckFunction(row) {
+    debugger;
+    for(i=0;i<valuearray.length;i++){
+     if(valuearray[i].rowid === row){
+        valuearray[i].status = !valuearray[i].status;
+        break;
+    }
     localStorage.setItem("savedArr", JSON.stringify(valuearray))
-    const storedValues = localStorage.getItem("savedArr");
-    console.log(storedValues)
+}
 }
 function displayValues(array) {
+    if (!array || array.length === 0) {
+        document.getElementById("tbody1").innerHTML = '<tr><td colspan="5" class="text-center text-warning p-4">No records found</td></tr>';
+        return;
+    }
     debugger;
     console.log('entered')
     console.log('initial', array)
@@ -34,59 +58,71 @@ function displayValues(array) {
         rows += '<td>' + (i + 1) + '</td>';
         rows += '<td>' + array[i].category + '</td>';
         rows += '<td>' + array[i].desc + '</td>';
-        rows += '<td ><div class="form-check form-check-success "><input type="checkbox" class="form-check-input" ' + (array[i].status ? 'checked' : '') + '  id="check' + i + '" onchange="CheckFunction(' + i + ')"></div></td>';
-        rows += '<td><button class="btn text-danger" onclick="DeleteRow(' + i + ')">Delete</button></td>';
+        rows += '<td ><div class="form-check form-check-success "><input type="checkbox" class="form-check-input" ' + (array[i].status ? 'checked' : '') + '  id="check' + i + '" onchange="CheckFunction(' +array[i].rowid+ ')"></div></td>';
+        rows += '<td><button class="btn text-danger" onclick="DeleteRow(' + array[i].rowid + ')">Delete</button></td>';
         rows += '</tr>';
     }
     document.getElementById('tbody1').innerHTML = rows;
 }
-function DeleteRow(item) {
+function DeleteRow(row) {
     debugger;
-    console.log('val in d ',valuearray)
-    valuearray.splice(item,1);
+    console.log('val in d ', valuearray)
+    valuearray.forEach((element,index) => {
+        if(element.rowid === row){
+            valuearray.splice(index, 1);
+        }
+    });
     localStorage.setItem("savedArr", JSON.stringify(valuearray));
-    
-   // displayValues(arrayName);
-   
-    if(completedFlag){
+
+    // displayValues(arrayName);
+
+    if (completedFlag) {
         CompletedClicked();
     }
-    else if(pendingFlag){
+    else if (pendingFlag) {
         PendingClicked()
     }
-    else{
+    else {
         displayValues(valuearray);
     }
-   
+
 }
 
-function CompletedClicked(){
-    completedFlag = true ;
+function CompletedClicked() {
+    completedFlag = true;
     pendingFlag = false;
     let CompletedItems = valuearray.filter(item => item.status === true);
     displayValues(CompletedItems);
 
 }
-function AllClicked(){
-    completedFlag = false ;
-    pendingFlag = false;    
+function AllClicked() {
+    completedFlag = false;
+    pendingFlag = false;
     displayValues(valuearray);
 }
-function PendingClicked(){
+function PendingClicked() {
     pendingFlag = true;
-    completedFlag = false ;
+    completedFlag = false;
     let PendingItems = valuearray.filter(item => item.status === false);
     displayValues(PendingItems);
 }
-function clearCompleted(){
-   for(i=0;i<valuearray.length;i++){
-    if(valuearray[i].status === true){
-        valuearray.splice(i)
-
-    }
-   }
+function clearCompleted() {
+    debugger;
+    valuearray.forEach((element,index) => {
+        if(element.status === true){
+            valuearray.splice(index,1)
+        } ;
+    });
     localStorage.setItem("savedArr", JSON.stringify(valuearray));
-    displayValues(valuearray);
+    if (completedFlag) {
+        CompletedClicked();
+    }
+    else if (pendingFlag) {
+        PendingClicked()
+    }
+    else {
+        displayValues(valuearray);
+    }
 
 }
 displayValues(valuearray);
